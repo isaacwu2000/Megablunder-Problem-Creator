b736d0f960dfff86cf28bd961e72f1ca0ed8148f
Isaac Wu <isaacwu2000@gmail.com>
2025-08-26 15:19:42 -0400

    finished json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,18 +1,32 @@
 import json, csv
 
-with open("problems/all_problems.csv", "r") as f:
-    problems = list(csv.DictReader(f))
+def format_problems_json(file_path, difficulty, synthetic=True):
+    with open(file_path, "r") as f:
+        try:
+            problems_json = json.load(f)
+        except json.decoder.JSONDecodeError:
+            return []
+        formatted_problems = []
+        for problem in problems_json:
+            problem["difficulty"] = difficulty
+            if synthetic:
+                problem["source"] = "AI + Human"
+            else:
+                problem["source"] = "Human"
+            formatted_problems.append(problem)
+    return formatted_problems
 
-with open("problems/hard_length_generated.json", "r") as f:
-    hard_length_generated_json = json.load(f)
-    hard_length_generated = []
-    for problem in hard_length_generated_json:
-        problem["difficulty"] = "hard_length"
-        problem["synthetic"] = True
-        hard_length_generated.append(problem)
+# Format all the problems into a unified dictionary format for CSV write
+hard_length = format_problems_json("human_examples/hard_length.json", "hard_length", synthetic=False)
+hard_trick = format_problems_json("human_examples/hard_trick.json", "hard_trick", synthetic=False)
+medium = format_problems_json("human_examples/medium.json", "medium", synthetic=False)
+hard_length_generated = format_problems_json("problems/hard_length_generated.json", "hard_length")
+hard_trick_generated = format_problems_json("problems/hard_trick_generated.json", "hard_trick")
+medium_generated = format_problems_json("problems/medium_generated.json", "medium")
 
+# Add all of the problems to all_problems.csv
 with open("problems/all_problems.csv", "w") as f:
-    problems += hard_length_generated
-    writer = csv.DictWriter(f, fieldnames = ["problem","answer","solution","category","difficulty","synthetic"])
+    problems = hard_length + hard_trick + medium + hard_length_generated + hard_trick_generated + medium_generated
+    writer = csv.DictWriter(f, fieldnames = ["problem","answer","solution","category","difficulty","source"])
     writer.writeheader()
     writer.writerows(problems)
