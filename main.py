3dc8538e7c6f24270a07472f0258e472a96b1ae7
Isaac Wu <isaacwu2000@gmail.com>
2025-08-26 16:06:48 -0400

    finished medium.txt prompt

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from model import generate_hard_length, generate_hard_trick
+from model import generate_hard_length, generate_hard_trick, generate_medium
 
 topics = ["A", "FRAG", "ROS", "PR", "AGR", "PAR", "CASE", "MM", "DM"]
 
 for topic in topics:
     for i in range(20):
-        generate_hard_trick(topic)
+        generate_medium(topic)
         print(topic, i)
-
-# todo: generate medium problems
