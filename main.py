9bfbfe34329d2e9abce5bd5ed28496b50109a61a
Isaac Wu <isaacwu2000@gmail.com>
2025-08-26 18:13:40 -0400

    finished problem generation!

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from model import generate_hard_length, generate_hard_trick, generate_medium
 
+for i in range(3):
+    generate_medium("FRAG")
+    print("FRAG", i)
+"""
 topics = ["A", "FRAG", "ROS", "PR", "AGR", "PAR", "CASE", "MM", "DM"]
 
-for topic in topics:
+for topic in topics[2:]:
     for i in range(20):
         generate_medium(topic)
         print(topic, i)
+"""
